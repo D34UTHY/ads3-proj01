b05c84ca8c2203687d899fe290e1a7cab1049fb6
Matheus Sant'Ana <matheuzinhu.santana@gmail.com>
2025-02-26 20:05:42 -0300

    Este é o primeiro commit

---
+++ src/script.js
@@ -0,0 +1,15 @@
+function calcularIdade() {
+
+    let anoDigitado = document.getElementById("anoNascimento").value
+
+    idade = 2025 - anoDigitado
+    
+    console.log(idade)
+
+    document.getElementById("resultado").innerHTML = `A idade é: ${idade}`
+}
+
+// Novas implementações
+// - Utilizar o ano atual para fazer a conta
+// - Aceitar apenas ano maior que 1900.
+// - Não permitir ano maior que o o input seja vazio
